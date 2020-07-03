e2eec1a43b0c14b6a8fd7ffc581b6cc697ef4b0d
Hoan Le <hoanle@xtaypro.com>
2020-07-04 00:57:37 +0700

    Add api fake user and get user list

--- src/components/user/userController.js
+++ src/components/user/userController.js
@@ -1,6 +1,7 @@
 const { catchAsync } = require("./../error/errorController");
 const AppError = require("./../error/appError");
 const User = require("./userModel");
+const faker = require("faker");
 
 exports.createUser = catchAsync(async (request, response, next) => {
   const { name, email, password } = request.body;
@@ -14,4 +15,44 @@ exports.createUser = catchAsync(async (request, response, next) => {
     status: "success",
     data: { user, token },
   });
+});
+
+exports.getUserList = catchAsync(async (request, response, next) => {
+  const { role } = request.body
+  const query = (role) ? {role: role} : {} 
+  const userList = await User.find(query).limit(20);
+  response.status(200).json({
+    status: 'success', 
+    data: { userList }
+  })
+});
+
+exports.fakeUsers = catchAsync(async (request, response, next) => {
+  let i = 1;
+  let { role, number } = request.body;
+  if (!role) role = "user";
+
+  if (!number) number = 10;
+
+  const intervalId = setInterval(async () => {
+    if (i < number) {
+      i++;
+      try {
+        const user = await User.create({
+          name: faker.name.findName(),
+          email: faker.internet.email(),
+          password: "123456789",
+          role: role,
+        });
+      } catch (error) {
+        console.log();
+      }
+    } else {
+      clearInterval(intervalId);
+      response.status(200).json({
+        status: "success",
+        message: `Created ${number} users`,
+      });
+    }
+  }, 1000);
 });
--- src/components/user/userRoutes.js
+++ src/components/user/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { createUser } = require("./userController");
+const { createUser, fakeUsers, getUserList } = require("./userController");
 
+router.route('/').get(getUserList);
 router.route("/register").post(createUser);
+router.route('/fakeUsers').post(fakeUsers);
 
 module.exports = router;
